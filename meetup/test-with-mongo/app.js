d092f8b4f543eb965068b02b910723a886d0fdad
MarAntonio82 <MarAntonio82@gmail.com>
2014-07-23 00:58:21 +0200

    proyecto ejemplos de node

--- meetup/test-with-mongo/app.js
+++ meetup/test-with-mongo/app.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var path = require('path');
 var osprey = require('osprey');
-
 var app = module.exports = express();
+var ObjectID = require('mongodb').ObjectID;
+var db;
+var MongoClient = require ('mongodb').MongoClient;
 
+//permite recoger la conexion a la base de datos
+MongoClient.connect('mongodb://localhost:27017/meetup',function(err,dbmongo){
+	if (err) throw err;
+		db = dbmongo;		
+	});
+
+	
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(express.compress());
@@ -11,75 +20,128 @@ app.use(express.logger('dev'));
 
 app.set('port', process.env.PORT || 3000);
 
-var users = new Array();
+//antes
+//var users = new Array();
 
-api = osprey.create('/api', app, {
-  ramlFile: path.join(__dirname, '/assets/raml/test.raml'),
-  logLevel: 'debug'  //  logLevel: off->No logs | info->Show Osprey modules initializations | debug->Show all
-});
 
- //Adding business logic to a valid RAML Resource
+//una vez implementada la API, ya no hace falta el fake
+//api = osprey.create('/api', app, {
+//  ramlFile: path.join(__dirname, '/assets/raml/test.raml'),
+//  logLevel: 'debug'  // logLevel: off->No logs | info->Show Osprey modules
+//						// initializations | debug->Show all
+//});
+
+ /**
+  * obtiene los usuarios
+  */
  app.get('/api/users', function(req, res) {
 
-   res.send({
-	    "result": {
-	        "info": "OK"
-	    },
-	    "data": users
-	});
+	  var params={};
+	   if (req.params){
+		   params=req.params;
+	   }
+		db.collection('users').find(params).toArray(function(err,users){
+			if (err)throw err;
+			 res.send({
+				    "result": {
+				        "info": "OK"
+				    },
+				    "data": users
+				});
+				
+		});
+	 
+	 
+  
  });
  
+ /**
+  * crea un usuario
+  */
  app.post('/api/users', function(req, res) {
 	   
+	   //utilizamos el cuerpo del mensaje para crear el objeto usuario
 	 	var user=req.body;
-	   users.push(user);
-	  
-	   res.send({
-		    "result": {
-		        "info": "OK"
-		    },
-		    "data": user
-		});
-	 });
+	   // antes
+	   // users.push(user);
+	   // ahora
+	 	db.collection('users').insert(user,function(err,user){
+						
+			   res.send({
+				    "result": {
+				        "info": "OK"
+				    },
+				    "data": user
+				});
+			 });
+ 	});
  
+ 
+ /**
+  * obtiene un usuario
+  */
  app.get('/api/users/:userid', function(req, res) {
 	   
-	 	var user=users[req.params.userid];
-	   
+	    // antes
+	   // var user=users[req.params.userid];
+	   //hay que convertir el id de usuario en un objectid de mongo
+	   var objectId = new ObjectID(req.params.userid);
 	  
-	   res.send({
-		    "result": {
-		        "info": "OK"
-		    },
-		    "data": user
-		});
-	 });
- 
+	   //findOne me permite obtener un solo objeto 
+	   db.collection('users').findOne({_id:objectId},function(err,user){
+			
+		   res.send({
+			    "result": {
+			        "info": "OK"
+			    },
+			    "data": user
+			});
+		 });
+ });
+
+/**
+ * modifica todo el usuario (no es una actualización parcial, si no total)
+ */ 
  app.put('/api/users/:userid', function(req, res) {
 	   
-	 	users[req.params.userid]=req.body;
-	   
+	 // antes
+	 // users[req.params.userid]=req.body;
+	   var objectId = new ObjectID(req.params.userid);
+	 
+	  var user = req.body;
+	 
 	  
-	   res.send({
-		    "result": {
-		        "info": "OK"
-		    },
-		    "data": users[req.params.userid]
-		});
-	 });
- 
+	 db.collection('users').update({_id:objectId},user,function(err,updated){
+			
+		   res.send({
+			    "result": {
+			        "info": "OK"
+			    },
+			    "data": updated
+			});
+		 });
+ });
+	   
+	 
+ /**
+  * permite borrar un usuario
+  */
  app.delete('/api/users/:userid', function(req, res) {
 	   
-	 	
-	 	users.splice(req.params.userid,1);
+	 var objectId = new ObjectID(req.params.userid);
+	 // antes
+	 // users.splice(req.params.userid,1);
 	   
+	 db.collection('users').remove({_id:objectId},function(err,user){
+			
+		 res.send({
+			    "result": {
+			        "info": "OK"
+			    },
+			    "data": user
+			});
+		 });
 	  
-	   res.send({
-		    "result": {
-		        "info": "OK"
-		    },
-		    "data": users
-		});
 	 });
  
  
